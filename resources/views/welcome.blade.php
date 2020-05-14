403186072b459cfd1c802dfcabae372aa427cdc2
Rafael Avalos <rafaelavalos@MacBook-Pro-de-Rafael.local>
2020-05-13 21:07:29 -0500

    modificacion 3ra

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,5 @@
 @section('contenido')
                 <div class="title m-b-md">
                     {{ $nombre ?? "invitado" }}
-                    {{ "hola" }}
                 </div>        
 @endsection
